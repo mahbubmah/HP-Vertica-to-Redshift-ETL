51fa49ada5e2bcd496ee30baea67179f8844e5b7
Md khirul islam <khirulislam.cse@gmail.com>
2018-02-20 11:09:39 +0600

    cleanup'

--- store.sh
+++ store.sh
@@ -1,2 +1,1 @@
-source env/bin/activate
-python sync.py   --aws_role_arn "arn:aws:iam::_________"   --tables "s3://ash.data/khirul/sqoop-script-2/tables.txt"   --target_s3_path "target s3 path"   --trgt_db_host "target database host"   --trgt_db_name "target database name"   --trgt_username "admin"   --trgt_password "****"   --trgt_port "5439" --degree_of_parallelism 1
+source env/bin/activate && python store.py --aws_role_arn "arn:aws:iam::324827999179:role/myRedshiftRole"   --tables "s3://ash.data/khirul/sqoop-script-2/tables.txt"   --target_s3_path "s3://ash.data/vertica/02-13"   --trgt_db_host "rsdata.c5y5lcyfmqhg.us-east-1.redshift.amazonaws.com"   --trgt_db_name "qsdata"   --trgt_username "admin"   --trgt_password "Vfr45tgb"   --trgt_port "5439" --degree_of_parallelism 1
