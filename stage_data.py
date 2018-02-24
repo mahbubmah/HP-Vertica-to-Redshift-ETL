c7476d75910bc7e3dd9fe3e37907ceb1c0e7921a
mahbub.apece <mahbub.apece@gmail.com>
2018-02-25 00:43:03 +0600

    resolve case sensetive issue for create avro file

--- stage_data.py
+++ stage_data.py
@@ -42,7 +42,9 @@ def get_table_desc(tables):
 
 def stage_src_data(table,s3_bucket_path,src_driver,src_db_url,src_username,src_password,number_of_mappers,split_column):
     print(s3_bucket_path)
-    destroy_s3_bucket(s3_bucket_path)    
+    pdb.set_trace()
+    destroy_s3_bucket(s3_bucket_path) 
+
     l_column_names=lower_table_column_names(table)
     query = "SELECT "+(', '.join(l_column_names))+" FROM "+table+"  where $CONDITIONS"
     print(query)
