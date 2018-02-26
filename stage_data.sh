a7a5516ca07843c6a2218418703a0b81c58e5477
kamrul <mailid127@gmail.com>
2018-02-26 17:16:24 +0600

    fix conflict

--- stage_data.sh
+++ stage_data.sh
@@ -1,7 +1,7 @@
 source env/bin/activate && \
 python stage_data.py --host "ec2-52-90-229-247.compute-1.amazonaws.com" \
--U "dbadmin" -P "vfr45tgb" -t "DimMktSource" -T "s3://ash.data/vertica/02-13"\
---src_driver "com.vertica.jdbc.Driver"\
+-U "dbadmin" -P "vfr45tgb" -t "DimMktSource" -T "s3://ash.data/vertica/02-13" \
+--src_driver "com.vertica.jdbc.Driver" \
 --src_db_url "jdbc:vertica://ec2-52-90-229-247.compute-1.amazonaws.com/sabredb"
 
 # ec2-52-90-229-247.compute-1.amazonaws.com
