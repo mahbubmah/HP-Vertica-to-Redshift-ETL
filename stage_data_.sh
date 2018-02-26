968d2cc3ea65a34244371362d679a647dc681417
kamrul <mailid127@gmail.com>
2018-02-26 18:35:16 +0600

    add no internet staging bash script

---
+++ stage_data_.sh
@@ -0,0 +1,5 @@
+source env/bin/activate && \
+python stage_data.py --host "10.0.2.114" \
+-U "dbadmin" -P "vfr45tgb" -d "uav01d1" -t "SHSCRSWHGSummary.DimMktSource" -T "s3://ash.data/vertica/02-13" \
+--src_driver "com.vertica.jdbc.Driver" \
+--src_db_url "jdbc:vertica://10.0.2.114/uav01d1"
