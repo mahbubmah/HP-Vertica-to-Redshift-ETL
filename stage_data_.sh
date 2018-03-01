04b38b758a0949a72cff1269984eba9d3e26c992
Md khirul islam <khirulislam.cse@gmail.com>
2018-03-01 10:32:11 +0600

    stage_data_.sh removed

--- stage_data_.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-source env/bin/activate && \
-python stage_data.py --host "10.0.2.114" \
--U "dbadmin" -P "vfr45tgb" -d "uav01d1" -t "SHSCRSWHGSummary.DimMktSource" -T "s3://ash.data/vertica/02-13" \
---src_driver "com.vertica.jdbc.Driver"
