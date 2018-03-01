f61420b9345fef4e56329d33db6ad2997481e546
Md khirul islam <khirulislam.cse@gmail.com>
2018-03-01 12:20:33 +0600

    logger for table added.

--- store.py
+++ store.py
@@ -1,4 +1,5 @@
 import sys
+import time
 sys.path.append('./env/lib/python2.7/site-packages/')
 sys.path.append('./env/lib64/python2.7/site-packages/')
 import psycopg2
@@ -47,7 +48,7 @@ def read_params(logger):
 
     return params
 
-def create_db_connection(hostname,database,username,password,port):
+def create_db_connection(logger,hostname,database,username,password,port):
     try:
         connection = psycopg2.connect(
             database = database,
@@ -57,45 +58,53 @@ def create_db_connection(hostname,database,username,password,port):
             port = port)
         return connection
     except:
-        print ("unable to connect to database")
+        logger.exception("Can't connect to redshift database.")
 
-def store(trgt_db_conn,table,s3_bucket_path,aws_role_arn):
-    cur = trgt_db_conn.cursor()
+def store(logger,trgt_db_conn,table,s3_bucket_path,aws_role_arn):
+    try:
+        cur = trgt_db_conn.cursor()
 
-    cur.execute("TRUNCATE "+table)
-    cur.execute("COMMIT;")
-    cmd_copy_to_trgt = "copy "+ table +" from '"+ s3_bucket_path +"'"+" iam_role '"+aws_role_arn+"'"+" format as avro 'auto' ACCEPTANYDATE DATEFORMAT 'YYYY-MM-DD' TIMEFORMAT 'epochmillisecs';"
-    cur.execute (cmd_copy_to_trgt)
-    cur.execute("COMMIT;")
-    print ("PROCESSING COMPLETE FOR TABLE : "+table)
+        logger.info("Truncating table")
+        cur.execute("TRUNCATE "+table)
+        cur.execute("COMMIT;")
+        logger.info("Truncate successful")
 
-def get_table_desc(tables):
-    if(tables.startswith("s3")):
-        download_s3_data(tables,os.getcwd())
-        file_name = tables.split("/")[-1]
-        table_file = open(file_name,'r')
-        list_table = list(table_file)
-        table_file.close()
-    else:
-        list_table= tables.split(",")
+        cmd_copy_to_trgt = "copy "+ table +" from '"+ s3_bucket_path +"'"+" iam_role '"+aws_role_arn+"'"+" format as avro 'auto' ACCEPTANYDATE DATEFORMAT 'YYYY-MM-DD' TIMEFORMAT 'epochmillisecs';"
+        logger.debug("Redshift copy command \n\n "+cmd_copy_to_trgt +"\n")
 
-    return list_table
+        logger.info("Executing redshift copy command")
+        cur.execute (cmd_copy_to_trgt)
+        logger.debug("Redshift copy command : "+cmd_copy_to_trgt)
+        cur.execute("COMMIT;")
+        logger.info("Redshift copy process finished")
+    except Exception as e:
+        logger.exception("Redshift copy process step error.")
 
 def _process(params,table):
-    table_name = table['name']
-
-    s3_bucket_path = params['target_s3_path'] + "/" + table_name + "/"
-    src_db_url = "jdbc:vertica://" + params['host'] + "/" + params['db_name']
-    password = params['password']
-    if not password:
-        password = ssm_pass()
-
-    connection = create_db_connection(params['host'] , params['db_name'], params['username'],password,params['port'])
+    logger=jobLogger(table['name'])
 
     try:
-       store(connection,table_name,s3_bucket_path,params['role_arn'])
+        process_start_time= time.time()
+        logger.info('Starting process...')
+        logger.info(table['name']+' process')
+        table_name = table['name']
+
+        s3_bucket_path = params['target_s3_path'] + "/" + table_name + "/"
+        logger.info('Processed file will save to - '+s3_bucket_path)
+
+        password = params['password']
+        if not password:
+            logger.info('Logging using ssm_pass')
+            password = ssm_pass()
+
+        connection = create_db_connection(logger,params['host'] , params['db_name'], params['username'],password,params['port'])
+        store(logger,connection,table_name,s3_bucket_path,params['role_arn'])
+
+        process_end_time= time.time()
+        logger.info('Process successfully completed.')
+        logger.info('Total time taken - '+str(round(process_end_time - process_start_time, 2))+' sec.')
     except:
-       print("ERROR PROCESSING TABLE : "+table_name)
+        logger.exception("Couldn't start process.")
 
 def store_data(params):
     for table in params['tables']:
