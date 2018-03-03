f81fa2b7e0e7c7b464653661e54e5e6f79b721ed
Md khirul islam <khirulislam.cse@gmail.com>
2018-03-03 16:03:44 +0600

    log added

--- cleaner.py
+++ cleaner.py
@@ -22,21 +22,23 @@ def read_params(logger):
     except Exception as e:
         logger.exception("Config file couldn't load")
 
-def upload_log_file(s3_bucket_path):
+def upload_log_file(s3_bucket_path,logger):
+    logger.info("Uploading log file")
     command = "aws s3 sync ./log " + s3_bucket_path + "/" + time.strftime('%Y%m%d/%H%M%S') + "/"
     subprocess.call(command, shell=True)
+    logger.info("Log file uploaded successfully")
 
 def clear_dir(path):
     shutil.rmtree(path)
 
-def _process(params):
-    upload_log_file(params['target_s3_path'])
-    clear_dir("log")
+def _process(params,logger):
+    upload_log_file(params['target_s3_path'],logger)
+    # clear_dir("log")
 
 if __name__ == '__main__':
     logger=jobLogger('root')
     try:
         params = read_params(logger)
-        _process(params)
+        _process(params,logger)
     except Exception as e:
         logger.exception("Couldn't start process.")
