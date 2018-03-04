b2a77d38e42ebd0da58add5294366e18d58bbf5a
Md khirul islam <khirulislam.cse@gmail.com>
2018-03-04 12:31:58 +0600

    stage data from tmp folder to original folder

--- cleaner.py
+++ cleaner.py
@@ -3,6 +3,9 @@ from utill import memoize, read_config
 from logger import *
 import shutil
 import subprocess
+import multiprocessing
+from functools import partial
+from multiprocessing import Pool
 
 @memoize
 def read_params(logger):
@@ -15,7 +18,15 @@ def read_params(logger):
         params = {}
 
         params['target_s3_path'] = config['aws']['s3_path']
+        params['tables'] = config['tables']
         logger.info("s3 bucket path - "+params['target_s3_path'])
+
+        max_process = multiprocessing.cpu_count()
+        if(max_process<=config['aws']['degree_of_parallelism']  or config['aws']['degree_of_parallelism'] <0):
+            logger.info ("Setting degree of parallelism to cpu count "+ str(max_process))
+            params['degree_of_parallelism'] = max_process
+        else:
+            params['degree_of_parallelism']= config['aws']['degree_of_parallelism']
 
         return params
 
@@ -31,7 +42,32 @@ def upload_log_file(s3_bucket_path,logger):
 def clear_dir(path):
     shutil.rmtree(path)
 
+def destroy_s3_folder(s3_bucket_path):
+    command = "aws s3 rm " + s3_bucket_path + " --recursive"
+    subprocess.call(command, shell=True)
+
+def sync_two_s3_folder(src,dest):
+    command = "aws s3 sync " + src + " "+ dest
+    subprocess.call(command, shell=True)
+
+def stage_temp_file(params,table):
+    table_name = table['name']
+    s3_bucket_path = params['target_s3_path'] + "/" + table_name + "/"
+    destroy_s3_folder(s3_bucket_path)
+    s3_bucket_path_tmp = params['target_s3_path'] + "/" + table_name + "_tmp/"
+    sync_two_s3_folder(s3_bucket_path_tmp,s3_bucket_path)
+    destroy_s3_folder(s3_bucket_path_tmp)
+
 def _process(params,logger):
+    max_process= multiprocessing.cpu_count()
+    if(max_process<=params['degree_of_parallelism']  or params['degree_of_parallelism'] <0):
+        pool = Pool(max_process)
+    else:
+        pool = Pool(params['degree_of_parallelism'])
+
+    sub_process=partial(stage_temp_file, params)
+    pool.map(sub_process, params['tables'])
+
     upload_log_file(params['target_s3_path'],logger)
     clear_dir("log")
 
