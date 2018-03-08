fb754d979aa77210fa4664a4fe4797e42347f457
Kamrul Hasan <kamrul@pridesys.com>
2018-03-08 11:29:12 +0000

    removing all files

--- stage_data.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-#export MACHINE_ENV=STAGE
-#export MACHINE_ENV=DEV
-#export MACHINE_ENV=PROD
-source env/bin/activate
-python stage_data.py
-
-# ec2-52-90-229-247.compute-1.amazonaws.com
-# jdbc:vertica://172.31.29.108/sabredb
